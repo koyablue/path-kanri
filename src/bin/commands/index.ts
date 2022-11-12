f317a347106eb4fede8467e156420c1f4801a296
koyaaoyama <aoyama.ko0920@gmail.com>
2022-11-12 20:57:22 +0900

    edit package.json. change build command

--- src/bin/commands/index.ts
+++ src/bin/commands/index.ts
@@ -8,6 +8,12 @@ const init = (dirPath: string, stubPath: string) => {
   console.log('dirPath:', dirPath)
   console.log('stubPath:', stubPath)
   console.log('__dirname:', __dirname)
+// npx pathkanri-publish
+// dirPath: /Users/koyaaoyama/projects/sandbox-reac-app/node_modules/path-kanri/build/src/bin/pathKanri
+// stubPath: /Users/koyaaoyama/projects/sandbox-reac-app/node_modules/path-kanri/build/src/bin/stubs/pathKanri.stub
+// __dirname: /Users/koyaaoyama/projects/sandbox-reac-app/node_modules/path-kanri/build/src/bin/commands
+
+
   // if (!fs.existsSync(dirPath)) {
   //   fs.mkdirSync(dirPath);
   // }
